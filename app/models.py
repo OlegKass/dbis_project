c7a3f879d73e883cffab3e41d6a389abfdd5d09d
OlegKass <44788664+OlegKass@users.noreply.github.com>
2021-11-21 16:01:43 +0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -13,23 +13,48 @@ class User(db.Model):
 	login = db.Column(db.String(128), nullable=False)
 	password = db.Column(db.String(64), nullable=False)
 	email = db.Column(db.String(128), nullable=False)
-	# ...
+	name = db.Column(db.String(128), nullable=False)
+    cookie = db.Column(db.String(128), nullable=False)
+    type = db.Column(db.String(128), nullable=False)
+    photo = db.Column(db.String(128), nullable=False)
 
 	def __repr__(self):
 		return f'<User {self.uid}>'
 
 
-class Channel(db.Model):
-	pass
+class Student(db.Model):
+    Student_id = db.Column(db.Integer, primary_key=True)
+    Group = db.Column(db.String(128), nullable=False)
+    Department = db.Column(db.String(128), nullable=False)
 
+class Professor(db.Model):
+    Professor_id = db.Column(db.Integer, primary_key=True)
+    Department = db.Column(db.String(128), nullable=False)
+
+class Channel(db.Model):
+    uid = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(128), nullable=False)
+    photo = db.Column(db.Integer, nullable=False)
 
 class Post(db.Model):
-	pass
+	Post_id = db.Column(db.Integer, primary_key=True)
+    Channel_id = db.Column(db.Integer, nullable=False)
+    Text = db.Column(db.String(128), nullable=False)
+    Attachments = db.Column(db.Integer, nullable=False)
+    Author_id = db.Column(db.Integer, nullable=False)
 
 
-class Message(db.Model):
-	pass
+class User_Channel(db.Model):
+    User_id = db.Column(db.Integer, primary_key=True)
+    Channel_id = db.Column(db.Integer, primary_key=True)
 
+class User_types(db.Model):
+    UserType_id = db.Column(db.Integer, primary_key=True)
+    Type = db.Column(db.String(128), nullable=False)
+
+class Attachment(db.Model):
+    uid = db.Column(db.Integer, primary_key=True)
+    path = db.Column(db.String(128), nullable=False)
 
 class Comment(db.Model):
 	comid = db.Column(db.Integer, primary_key=True)
